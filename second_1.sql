6438114b5a56e9d791187f374d68cd45dfce4dea
akorniienko04 <akorniienko@kse.org.ua>
2025-10-22 19:14:59 +0300

    first

--- second_1.sql
+++ second_1.sql
@@ -1,1 +1,34 @@
-CREATE DATABASE SQL_2;
+
+USE SQL_2;
+
+
+-- CREATE TABLE customers (
+--     customer_id INT PRIMARY KEY,
+--     name VARCHAR(100),
+--     email VARCHAR(100),
+--     country VARCHAR(100),
+--     registration_date DATETIME
+-- );
+
+
+-- CREATE TABLE orders (
+--     order_id INT PRIMARY KEY,
+--     customer_id INT,
+--     order_date DATETIME,
+--     total_amount DECIMAL(10,2),
+--     status VARCHAR(50)
+-- );
+
+
+-- CREATE TABLE order_items (
+--     item_id INT PRIMARY KEY,
+--     order_id INT,
+--     product_name VARCHAR(100),
+--     quantity INT,
+--     price DECIMAL(10,2)
+-- );
+
+
+
+
+
