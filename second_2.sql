fc6eb2f692b37ea8e4423471dfb4d5fc00515153
akorniienko04 <akorniienko@kse.org.ua>
2025-10-22 23:41:28 +0300

    last

--- second_2.sql
+++ second_2.sql
@@ -1,1 +1,28 @@
 USE SQL_2;
+
+SELECT
+    c.name,
+    c.email,
+    c.country,
+    o.order_date,
+    o.total_amount,
+    i.product_name,
+    i.quantity,
+    i.price
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items i ON o.order_id = i.order_id
+WHERE o.status = 'Completed'
+  AND i.price > (
+      SELECT AVG(price)
+      FROM order_items
+  )
+  AND c.country IN (
+      SELECT DISTINCT country
+      FROM customers
+      WHERE country IS NOT NULL
+  )
+ORDER BY o.order_date DESC;
+
+
+
--- second_3.sql
+++ second_3.sql
@@ -1,1 +1,41 @@
 USE SQL_2;
+
+
+
+
+
+
+
+explain
+
+WITH
+order_counts AS(
+    SELECT customer_id, COUNT(*) AS orders_total
+    FROM orders
+    GROUP BY customer_id
+),
+avg_price_per_order AS(
+    SELECT order_id, AVG(price) AS avg_price_for_order
+    FROM order_items
+    GROUP BY order_id
+)
+
+SELECT
+    c.customer_id,
+    c.name,
+    c.country,
+    o.order_date,
+    i.product_name,
+    i.price,
+    oc.orders_total,
+    ap.avg_price_for_order
+FROM customers c
+JOIN 
+orders o ON c.customer_id = o.customer_id
+JOIN order_items i ON o.order_id = i.order_id
+LEFT JOIN order_counts oc ON c.customer_id = oc.customer_id
+LEFT JOIN avg_price_per_order ap ON o.order_id = ap.order_id
+WHERE o.status = 'Completed'
+ORDER BY o.order_date DESC;
+
+
